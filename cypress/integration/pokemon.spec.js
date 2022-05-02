f6bc2af9b0cb20429f8d9e45db0adbcec2c699cf
Hossein movahed <hossein.movahed@monese.com>
2022-05-02 13:45:58 +0300

    another try

--- cypress/integration/pokemon.spec.js
+++ cypress/integration/pokemon.spec.js
@@ -1,13 +1,9 @@
-// describe('Pokedex', function () {
-//   it('front page can be opened', function () {
-//     cy.visit('http://localhost:5000')
-//     cy.contains('ivysaur')
-//     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-//   })
-
-//   it('can open a card', function () {
-//     cy.visit('http://localhost:5000')
-//     cy.contains('ivysaur').click()
-//     cy.contains('chlorophyll')
-//   })
-// })
+describe('Pokédex', () => {
+    it('front page can be opened', () => {
+        cy.visit('http://localhost:5000')
+        cy.contains('ivysaur')
+        cy.contains(
+            'Pokémon and Pokémon character names are trademarks of Nintendo.'
+        )
+    })
+})
